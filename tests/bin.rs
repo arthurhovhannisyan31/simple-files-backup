843a542f71b38a3e86a7af9a542401ebd3968fd3
Arthur Hovhannisyan <arthurhovhannisyan31@gmail.com>
2025-09-11 12:47:47 +0400

    add target build to code validation workflow, fix mac and windows bin tests

--- tests/bin.rs
+++ tests/bin.rs
@@ -10,17 +10,16 @@ mod test_bin {
   use crate::helpers::{setup_config_file, setup_dirs, setup_files};
   use crate::stubs::get_file_path_stubs;
 
-  #[cfg(unix)]
   #[test]
   fn test_valid_setup() {
     let cur_dir = env::current_dir().unwrap();
     let (root_dir, source_dir, target_dir) = setup_dirs();
     let file_paths: Vec<&str> = get_file_path_stubs();
-    let bin_path = cur_dir.join("bin/simple-files-backup");
+    let bin_path = cur_dir.join("target/release/simple-files-backup");
     let config_file_path = root_dir.join("config.json");
     let log_file_path = root_dir.join("log.txt");
 
-    let mut cmd = Command::cargo_bin(bin_path.to_str().unwrap()).unwrap();
+    let mut cmd = Command::new(bin_path.to_str().unwrap());
 
     setup_files(&source_dir, &file_paths);
     setup_config_file(
@@ -32,27 +31,25 @@ mod test_bin {
       Some(log_file_path),
     );
 
-    cmd.arg("--config").arg(config_file_path).assert().success();
+    cmd.arg("-c").arg(config_file_path).assert().success();
 
     root_dir.close().unwrap();
   }
 
-  #[cfg(unix)]
   #[test]
   fn test_missing_config_file() {
     let cur_dir = env::current_dir().unwrap();
     let (root_dir, source_dir, _target_dir) = setup_dirs();
     let file_paths: Vec<&str> = get_file_path_stubs();
-    let bin_path = cur_dir.join("bin/simple-files-backup");
+    let bin_path = cur_dir.join("target/release/simple-files-backup");
     let config_file_path = root_dir.join("config.json");
 
     let mut cmd = Command::cargo_bin(bin_path.to_str().unwrap()).unwrap();
 
     setup_files(&source_dir, &file_paths);
 
-    cmd.arg("--config").arg(config_file_path).assert().failure();
+    cmd.arg("-c").arg(config_file_path).assert().failure();
 
     root_dir.close().unwrap();
   }
-  // TODO Write os specific tests
 }
