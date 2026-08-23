a8368a5e64a69abdb4e5d3977a7608cf032ed4ff
Arthur Hovhannisyan <arthurhovhannisyan31@gmail.com>
2026-08-23 14:32:16 +0400

    prevent app crashing on log whiting errors

--- src/modules/logs.rs
+++ src/modules/logs.rs
@@ -4,45 +4,25 @@ use std::path::PathBuf;
 
 use chrono::Local;
 
-use crate::modules::constants::FSErrors;
-
+/// Writes a log entry to `log_path`. Logging failures must never abort the
+/// backup, so any error is reported to stderr instead of panicking (a panic
+/// here would mask the very error we were asked to record).
 pub fn write_logs(log_path: &PathBuf, log_str: &str) {
-  let mut file = File::options()
-    .create(true)
-    .append(true)
-    .open(log_path)
-    .unwrap_or_else(|err| {
-      panic!(
-        "{:#?}",
-        FSErrors::NotFound {
-          source_path: String::from(log_path.to_str().unwrap()),
-          err
-        }
-      )
-    });
-
   let mut new_content = String::from("----\n");
   new_content.push_str(format!("{}\n", Local::now().to_rfc2822()).as_str());
   new_content.push_str(log_str);
 
-  file
-    .write_all(new_content.as_bytes())
-    .unwrap_or_else(|err| {
-      panic!(
-        "{:#?}",
-        FSErrors::WriteLogsError {
-          target_path: String::from(log_path.to_str().unwrap()),
-          err
-        }
-      )
-    });
-  file.sync_all().unwrap_or_else(|err| {
-    panic!(
-      "{:#?}",
-      FSErrors::FileSyncError {
-        source_path: String::from(log_path.to_str().unwrap()),
-        err
-      }
+  if let Err(err) = try_write_logs(log_path, &new_content) {
+    eprintln!(
+      "Failed writing logs to `{}`: {err}\n{new_content}",
+      log_path.to_string_lossy()
     );
-  });
+  }
+}
+
+fn try_write_logs(log_path: &PathBuf, content: &str) -> std::io::Result<()> {
+  let mut file = File::options().create(true).append(true).open(log_path)?;
+  file.write_all(content.as_bytes())?;
+  file.sync_all()?;
+  Ok(())
 }
